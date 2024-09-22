330cf5ab664ce3b7f9899715f9cd9c2b673ee69d
WilliamRagstad <william.ragstad@gmail.com>
2024-09-22 20:02:50 +0200

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::io::Write;
 
+use crossterm::{
+    style::{Attribute, Color, SetAttribute, SetForegroundColor},
+    ExecutableCommand,
+};
 use crossterm_cursor::{cursor, TerminalCursor};
 use types::Formula;
 
@@ -14,10 +18,11 @@ mod types;
 fn main() {
     println!("Welcome to the SAT Solver!");
     let mut cursor = cursor();
+    let mut stdout = std::io::stdout();
     let mut solver = solvers::Dfs;
     let mut style = PrintStyle::Normal;
     loop {
-        let input = read_line(&mut cursor);
+        let (input, start) = read_line(&mut cursor);
         match input.trim() {
             "" => (),
             "exit" => break,
@@ -50,14 +55,20 @@ fn main() {
                 let Some(formula) = parser::parse(expr) else {
                     continue;
                 };
-                update_line(&input, &formula, &mut cursor, &style);
+                update_line(&input, start, &formula, &mut cursor, &style);
                 let solutions = solver::solve_all(&formula, &solver);
                 if !solutions.is_empty() {
-                    if solutions.len() == 1 {
-                        print!("Satisfiable: ");
+                    stdout.execute(SetForegroundColor(Color::DarkGrey)).unwrap();
+                    stdout.execute(SetAttribute(Attribute::Italic)).unwrap();
+                    print!("\n  Satisfiable");
+                    if solutions.len() > 1 {
+                        print!(" ({})", solutions.len());
+                        println!(": ");
                     } else {
-                        println!("Satisfiable ({} solutions): ", solutions.len());
+                        print!(": ");
                     }
+                    stdout.execute(SetForegroundColor(Color::Reset)).unwrap();
+                    stdout.execute(SetAttribute(Attribute::Reset)).unwrap();
                     for solution in &solutions {
                         if solutions.len() > 1 {
                             print!("  ");
@@ -65,32 +76,39 @@ fn main() {
                         style.print_solution(solution);
                     }
                 } else {
-                    println!("Unsatisfiable");
+                    stdout.execute(SetForegroundColor(Color::DarkGrey)).unwrap();
+                    stdout.execute(SetAttribute(Attribute::Italic)).unwrap();
+                    println!("\n  Unsatisfiable");
+                    stdout.execute(SetForegroundColor(Color::Reset)).unwrap();
+                    stdout.execute(SetAttribute(Attribute::Reset)).unwrap();
                 }
             }
         }
     }
 }
 
-fn read_line(cursor: &mut TerminalCursor) -> String {
+fn read_line(cursor: &mut TerminalCursor) -> (String, (u16, u16)) {
     println!();
     let mut input = String::new();
     print!("> ");
     std::io::stdout().flush().unwrap();
-    if let Err(err) = cursor.save_position() {
-        eprintln!("Failed to save cursor position: {}", err);
-    }
+    let start = cursor.pos().unwrap();
     std::io::stdin().read_line(&mut input).unwrap();
-    input
+    (input, start)
 }
 
-fn update_line(input: &str, formula: &Formula, cursor: &mut TerminalCursor, style: &PrintStyle) {
-    if let Err(err) = cursor.restore_position() {
-        eprintln!("Failed to restore cursor position: {}", err);
-        return; // Skip the rest of the function
-    }
+fn update_line(
+    input: &str,
+    start: (u16, u16),
+    formula: &Formula,
+    cursor: &mut TerminalCursor,
+    style: &PrintStyle,
+) {
+    cursor.goto(start.0, start.1 - 1).unwrap();
     // Clear the current line with length of the input
     print!("{}", " ".repeat(input.len()));
-    cursor.restore_position().unwrap();
+    std::io::stdout().flush().unwrap();
+    cursor.goto(start.0, start.1 - 1).unwrap();
     style.print_formula(formula);
+    std::io::stdout().flush().unwrap();
 }
